d303abce84f2675c5b2893b7ad0cf55182e3b08e
Johannes Roth <johannes.roth18199@gmail.com>
2026-07-08 13:01:47 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,9 +1,14 @@
 
-./sim --dt 0.2 --nx 32 --ny 32 --nz 1 --eta 0.01 --output-folder run1
-./sim --dt 0.2 --nx 32 --ny 32 --nz 1 --eta 0.01 --output-folder run2
-./sim --dt 0.2 --nx 32 --ny 32 --nz 1 --eta 0.01 --output-folder run3
-./sim --dt 0.2 --nx 32 --ny 32 --nz 1 --eta 0.01 --output-folder run4
-./sim --dt 0.2 --nx 32 --ny 32 --nz 1 --eta 0.01 --output-folder run5
-./sim --dt 0.2 --nx 32 --ny 32 --nz 1 --eta 0.01 --output-folder run6
-./sim --dt 0.2 --nx 32 --ny 32 --nz 1 --eta 0.01 --output-folder run7
-./sim --dt 0.2 --nx 32 --ny 32 --nz 1 --eta 0.01 --output-folder run8
+./sim --dt 0.5 --output-every-n-steps 10 --sim-time 2000 --nx 32 --ny 32 --nz 32 --eta 0.01 --eta-uv-cutoff 3.0 --no-ideal-step --seed 1
+./sim --dt 0.5 --output-every-n-steps 10 --sim-time 2000 --nx 32 --ny 32 --nz 32 --eta 0.01 --eta-uv-cutoff 3.0 --no-ideal-step --seed 2
+./sim --dt 0.5 --output-every-n-steps 10 --sim-time 2000 --nx 32 --ny 32 --nz 32 --eta 0.01 --eta-uv-cutoff 3.0 --no-ideal-step --seed 3
+./sim --dt 0.5 --output-every-n-steps 10 --sim-time 2000 --nx 32 --ny 32 --nz 32 --eta 0.01 --eta-uv-cutoff 3.0 --no-ideal-step --seed 4
+./sim --dt 0.5 --output-every-n-steps 10 --sim-time 2000 --nx 32 --ny 32 --nz 32 --eta 0.01 --eta-uv-cutoff 3.0 --no-ideal-step --seed 5
+
+python scripts/compute_jpx_time_correlator.py --nkx 0 --nky 1 --nkz 0 --run-dir sim-Nx32Ny32Nz32dt5eta0.01Lam3seed1x
+python scripts/compute_jpx_time_correlator.py --nkx 0 --nky 1 --nkz 0 --run-dir sim-Nx32Ny32Nz32dt5eta0.01Lam3seed2x
+python scripts/compute_jpx_time_correlator.py --nkx 0 --nky 1 --nkz 0 --run-dir sim-Nx32Ny32Nz32dt5eta0.01Lam3seed3x
+python scripts/compute_jpx_time_correlator.py --nkx 0 --nky 1 --nkz 0 --run-dir sim-Nx32Ny32Nz32dt5eta0.01Lam3seed4x
+python scripts/compute_jpx_time_correlator.py --nkx 0 --nky 1 --nkz 0 --run-dir sim-Nx32Ny32Nz32dt5eta0.01Lam3seed5x
+
+python scripts/plot_jpx_time_correlator.py obs-Nx32Ny32Nz32dt5eta0.01Lam3x --ylim -0.2 1.2
